24f6c417285471791c2ce1f888f488b3ceb721c6
unknown <toropov.nikita2004@gmail.com>
2023-03-20 16:38:12 +0100

    Implement markRead function

---
+++ index.js
@@ -0,0 +1,28 @@
+let notificationCount = 0;
+
+
+window.onload = () => {
+    notificationCount = document.getElementsByClassName("new-notification").length
+    const notifications = Array.from(document.getElementsByClassName("new-notification"));
+    for(let notification of notifications){
+        notification.addEventListener("click", () => markRead(notification));
+    }
+    setNotificationsLabel();
+}
+function markAllRead(){
+    const notifications = Array.from(document.getElementsByClassName("new-notification"));
+    for(let notification of notifications){
+        markRead(notification);
+    }
+}
+function markRead(notification){
+    notification.classList.remove("new-notification");
+    notification.classList.add("notification");
+    notification.getElementsByClassName("red-dot")[0].remove();
+    --notificationCount;
+    setNotificationsLabel();
+    notification.removeEventListener("click", markRead);
+}
+function setNotificationsLabel(){
+    document.getElementsByClassName("notifications-count")[0].textContent = notificationCount;
+}
